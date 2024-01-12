83aa8a0b81f2355b40bf7d5f9d1fc5c0cae73597
Lennard Harteveld <s1145074@student.hsleiden.nl>
2024-01-12 13:55:12 +0100

    update

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ function checkPinCode() {
     var correctPin = "1234"; // Replace with your correct pin code
 
     if (enteredPin === correctPin) {
+        closeAlert();
         window.location.href = "https://s1145074.github.io/inpixpe/";
     } else {
-        alert("Incorrect Pin Code. Please try again.");
+        showAlert();
     }
 }
+
+var input = document.getElementById("pinCode");
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("check").click();
+  }
+});
+
+function home() {
+    window.location.href = "index.html"
+}
+
+function showAlert() {
+    document.getElementById('custom-alert').style.display = 'block';
+    document.getElementById('alert-message').innerHTML = 'Code niet herkend. Probeer opnieuw!';
+  }
+
+  function closeAlert() {
+    document.getElementById('custom-alert').style.display = 'none';
+  }
