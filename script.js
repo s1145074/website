c04c179b93c76e0fd7d1c962d9a66b7d8a43b7dc
Lennard Harteveld <s1145074@student.hsleiden.nl>
2024-01-18 18:11:41 +0100

    last

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-var input = document.getElementById("input");
-
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("check").click();
-  }
-});
-
-
-function home() {
-    window.location.href = "index.html"
-}
-
-
-function check() {
-  var user_input = document.getElementById("input_code").value;
+function check_input() {
+  var user_input = document.getElementById("input_box").value;
+  var message_element = document.getElementById("message");
 
   if (user_input === "1234") {
-      showAlert("Success! Code is valid.");
-  } else {
-      showAlert("Error! Please enter the correct code (1234).");
+    reset_message()
+    window.location.href = "https://www.google.nl/";
+  }
+  else if (user_input === "rick") {
+    reset_message()
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+  else {
+      document.getElementById('message_container').style.display = 'block';
+      message_element.innerText = "Code niet herkend! Probeer opnieuw.";
   }
 }
 
-function showAlert(message) {
-  document.getElementById("alert-message").innerText = message;
-  document.getElementById("custom-alert").style.display = "block";
-  setTimeout(function() {
-      document.getElementById("custom-alert").style.display = "none";
-  }, 3000);
+function reset_message() {
+  document.getElementById('message').innerText = '';
+  document.getElementById('message_container').style.display = 'none';
+}
+
+function home() {
+  reset_message()
+  window.location.href = "index.html";
 }
